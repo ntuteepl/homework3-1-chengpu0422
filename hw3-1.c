e60ccbc971a4e0d56e1d376e0e2dcf416669208d
chengpu0422 <146065192+chengpu0422@users.noreply.github.com>
2023-10-23 23:41:41 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,m;
+    scanf("%d",&n);
+
+    for ( m=2;m<n;m++)
+    {
+     if((n%m)==0)
+        {
+        printf("NO");
+        break;
+        }
+    }
+    if((n==m))
+        {
+            printf("YES");
+
+
+        }
+    }
+
+
 
