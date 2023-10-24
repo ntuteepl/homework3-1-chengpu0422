4651c30a9a2de33ecb21a252e37127b7ba0bba39
chengpu0422 <146065192+chengpu0422@users.noreply.github.com>
2023-10-24 09:31:03 +0800

    Update hw3-4.c

--- hw3-4.c
+++ hw3-4.c
@@ -1,50 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-  int a1,a2,b1,b2,c1,c2,d1,d2,e1,e2,A,B;
-  scanf("%d",&a1);
-  for(int x=0;x<2;x++)
-  {
-      x=0;
-      scanf("%d",&a2);
-      if(a2==0)
-      {
-          break;
-      }
-      A=0;
-      B=0;
-      e1=a1;
-      e2=a2;
-      for(int i=0;i<4;i++)
-        {
-        b1=e1%10;
-        b2=e2%10;
-        if(b1==b2)
-        {
-            A++;
-        }
-        else
-        {
-            d1=a1;
-            d2=a2;
-            for(int j=0;j<4;j++)
-            {
-                c1=d1%10;
-                c2=d2%10;
+int main(){
+    int a, b;
+    scanf("%d", &a);
+    int A[4] = {a/1000,(a%1000)/100,(a%100)/10,a%10};
+    while(1){
+    scanf("%d", &b);
+    int B[4] = {b/1000,(b%1000)/100,(b%100)/10,b%10};
+    int c = 0;
+    int d = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (A[i] == B[j] && i != j) {
+                c += 1;
             }
-            if(c1==b2)
-                {
-                B++;
-                }
-            d1/=10;
-            d2/=10;
+            if (A[i] == B[j] && i == j) {
+                d += 1;
+            }
         }
-        e1/=10;
-        e2/=10;
-        }
-        printf("%dA%dB",A,B);
-  }
+    }
+    printf("%dA%dB\n", d, c);
+    if (b==0){
+        break;
+    }
+    }
+    return 0;
 }
-
